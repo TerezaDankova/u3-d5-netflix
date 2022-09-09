fcbc334e9f80589b44569d19beed7b809647e910
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-09-09 16:49:51 +0200

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ class Home extends Component {
             <div className="row no-gutters" style={{rowGap: "20px", width: "100%"}}>
                   <div className="col-sm-6 col-md-12 col-lg-12 col-xl-12">
                         <div className="movie-card">
-          
+                        <h3 className="text-left ml-4 pl-3 mt-4 " style={{color: "white", fontSize: "18px", fontWeight: "bold"}}>Harry Potter</h3>
                         <Gallery style={{fontSize: "20px"}} keyword="Harry Potter" />
                        </div>
                   </div>
@@ -21,19 +21,24 @@ class Home extends Component {
             <div className="row no-gutters mt-5" style={{rowGap: "20px", width: "100%"}}>
                   <div className="col-sm-6 col-md-12 col-lg-8 col-xl-12">
                         <div className="movie-card">
+                        <h3 className="text-left ml-4 pl-3 mt-4 " style={{color: "white", fontSize: "18px", fontWeight: "bold"}}>The Lord of the Rings</h3>
                         <Gallery keyword="Lord of the rings" />
                        </div>
                   </div>
             </div>
+          
             <div className="row no-gutters mt-5" style={{rowGap: "20px", width: "100%"}}>
                   <div className="col-sm-6 col-md-12 col-lg-8 col-xl-12">
                         <div className="movie-card">
+                        <h3 className="text-left ml-4 pl-3 mt-4 " style={{color: "white", fontSize: "18px", fontWeight: "bold"}}>The Twilight</h3>
                         <Gallery keyword="twilight" />
                        </div>
                   </div>
             </div>
        
       </Container>
+
+      
     );
   }
 }
